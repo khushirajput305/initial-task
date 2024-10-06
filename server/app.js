fe213072e6404d7a9b172e4b736ba28dcb314b9e
khushirajput305 <khushinurawat305@gmail.com>
2024-10-06 20:45:20 +0530

    create node-app

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT,()=>{
+    console.log(
+        `server is running in ${process.env.DEV_MODE} Mode on port ${process.env.PORT}`
+  
+    )
+})
